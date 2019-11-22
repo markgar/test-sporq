0b9c6f61b4644bdec9c0b59451bc3d037877d0f5
Mark Garner <markgarner@Marks-MacBook-Pro.local>
2019-11-22 14:52:47 -0600

    restore ps1

---
+++ azuredeploy.ps1
@@ -0,0 +1,22 @@
+# Declare your common properties
+$commonProperties = Get-SpqCommonProperties
+$commonProperties.EnvironmentName = "dev"
+$commonProperties.ApplicationCode = "q4k"
+
+# Grab an empty base template
+$baseTemplate = Get-SpqBaseTemplate
+
+# Create Storage Account
+$myStorageAccount = Get-SpqStorageAccount -CommonProperties $commonProperties -Location "centralus" -StorageAccessTier "Standard_LRS" -StorageTier "Standard"
+
+# Add to Template
+$baseTemplate.resources += $myStorageAccount
+
+# create the ./out directory
+New-Item -ItemType Directory -Force -Path './out'
+
+# convert the template object to a json string and output to disk
+$baseTemplateAsJson = $baseTemplate | ConvertTo-Json -Depth 10
+
+# output to ./out directory so I can ignore it in .gitignore
+$baseTemplateAsJson | Out-File './out/azuredeploy.json'
