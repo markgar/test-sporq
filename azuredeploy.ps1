cfde76bcf4fb3f07b4eb42bdb6206b4725fc3a59
Mark Garner <markgarner@Marks-MacBook-Pro.local>
2019-11-25 07:56:45 -0600

    simplify

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -11,11 +11,5 @@ $myStorageAccount = Get-SpqStorageAccount -ApplicationCode $applicationCode -Env
 # Add to Template
 $baseTemplate.resources += $myStorageAccount
 
-# create the ./out directory - no need to output results to console (Out-Null)
-New-Item -ItemType Directory -Force -Path './out' | Out-Null
-
-# convert the template object to a json string and output to disk
-$baseTemplateAsJson = $baseTemplate | ConvertTo-Json -Depth 10
-
 # output to ./out directory so I can ignore it in .gitignore
-$baseTemplateAsJson | Out-File './out/azuredeploy.json'
+$baseTemplateAsJson | Out-File './azuredeploy.json'
