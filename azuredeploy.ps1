0a2eb0bf94dc7b4edf0df0d6e51154460d5b7429
Mark Garner <markgarner@Marks-MacBook-Pro.local>
2019-11-25 08:20:07 -0600

    simplify

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -1,12 +1,8 @@
-# Declare your common properties
-$environmentName = "dev"
-$applicationCode = "q4k"
-
 # Grab an empty base template
 $baseTemplate = Get-SpqBaseTemplate
 
 # Create Storage Account
-$myStorageAccount = Get-SpqStorageAccount -ApplicationCode $applicationCode -EnvironmentName $environmentName -Location "centralus" -StorageAccessTier "Standard_LRS" -StorageTier "Standard"
+$myStorageAccount = Get-SpqStorageAccount -ApplicationCode "kd8" -EnvironmentName "dev" -Location "centralus" -StorageAccessTier "Standard_LRS" -StorageTier "Standard"
 
 # Add to Template
 $baseTemplate.resources += $myStorageAccount
