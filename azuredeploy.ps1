32c7729bf4e22c4e760621847055ba9d0201e112
Mark Garner <markgarner@Marks-MacBook-Pro.local>
2019-11-25 07:54:04 -0600

    remove commonproperties

--- azuredeploy.ps1
+++ azuredeploy.ps1
@@ -1,13 +1,12 @@
 # Declare your common properties
-$commonProperties = Get-SpqCommonProperties
-$commonProperties.EnvironmentName = "dev"
-$commonProperties.ApplicationCode = "q4k"
+$environmentName = "dev"
+$applicationCode = "q4k"
 
 # Grab an empty base template
 $baseTemplate = Get-SpqBaseTemplate
 
 # Create Storage Account
-$myStorageAccount = Get-SpqStorageAccount -CommonProperties $commonProperties -Location "centralus" -StorageAccessTier "Standard_LRS" -StorageTier "Standard"
+$myStorageAccount = Get-SpqStorageAccount -ApplicationCode $applicationCode -EnvironmentName $environmentName -Location "centralus" -StorageAccessTier "Standard_LRS" -StorageTier "Standard"
 
 # Add to Template
 $baseTemplate.resources += $myStorageAccount
